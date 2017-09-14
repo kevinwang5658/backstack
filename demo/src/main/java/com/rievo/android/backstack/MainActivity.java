392d9dc77fe6737894f178413f98b35e86a04c64
Kevin Wang <kevinwang5658@gmail.com>
2017-09-14 09:31:09 -0400

    wiped clean

--- demo/src/main/java/com/rievo/android/backstack/MainActivity.java
+++ demo/src/main/java/com/rievo/android/backstack/MainActivity.java
@@ -1,13 +1,7 @@
 package com.rievo.android.backstack;
 
-import android.graphics.Color;
-
-import com.rievo.android.library.ClusterBackStack;
-import com.rievo.android.library.ClusterBackStackImpl;
-import com.rievo.android.library.LinearBackStack;
-import com.rievo.android.library.BackStackManager;
-
 import android.rievo.com.backstack.R;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -16,13 +10,13 @@ import android.widget.RelativeLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Simple BackStack";
 
     @BindView(R.id.root) RelativeLayout root;
+    @BindView(R.id.view_pager) ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +25,10 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        BackStackManager.install(this);
-        LinearBackStack.create(TAG, root, (inflater, container) -> {
-            return new ViewGroup1(inflater.getContext());
-        });
-        BackStackManager.setRootBackStack(LinearBackStack.get(TAG));
-
     }
 
 
     @Override
     public void onBackPressed() {
-        BackStackManager.getBackStackManager().goBack();
     }
 }
